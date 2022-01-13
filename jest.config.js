46b19b0193246eadd15ba472f4e893c409a8640b
Mauricio F <mauriciofsnts@gmail.com>
2022-01-13 17:51:56 -0300

    chore: config jest

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  roots: ['<rootDir>/src'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**'
+  ],
+  coverageDirectory: 'coverage',
+  coverageProvider: 'babel',
+  testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest'
+  }
+
+}
--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-  roots: ['<rootDir>/src'],
-  collectCoverage: true,
-  coverageDirectory: 'coverage',
-  testEnvironment: 'node',
-  transform: {
-    '.+\\.ts$': 'ts-jest'
-  }
-}
